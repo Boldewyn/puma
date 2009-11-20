ea0e9a533e6fffdcaef0c14b818aa3afa1c91b13
Manuel Strehl <puma@manuel-strehl.de>
2009-11-20 10:25:41 +0100

    added change for bJustLoggedOut (silly hungarian notation)

--- aigaionengine/libraries/userlogin.php
+++ aigaionengine/libraries/userlogin.php
@@ -266,18 +266,20 @@ class UserLogin {
         $CI->latesession->set('USERLOGIN', $this);
 
         try {
-            $CI->load->library('login_puma');
-            $loginInfo = $CI->login_puma->getLoginInfo();
-            if (isset($_POST['loginName']) && $_POST['loginName'] == "admin") {
-                // do nothing, proceed to local login
-            } elseif (isset($loginInfo['login']) && $loginInfo['login'] !== '') {
-                $this->internalLogin($loginInfo['login'], $loginInfo['password'], true, true);
-                return;
-            } elseif (isset($loginInfo['error'])) {
-                appendErrorMessage($loginInfo['error']);
-                return 1;
-            } else {
-                return 1;
+            if (! $this->bJustLoggedOut) {
+                $CI->load->library('login_puma');
+                $loginInfo = $CI->login_puma->getLoginInfo();
+                if (isset($_POST['loginName']) && $_POST['loginName'] == "admin") {
+                    // do nothing, proceed to local login
+                } elseif (isset($loginInfo['login']) && $loginInfo['login'] !== '') {
+                    $this->internalLogin($loginInfo['login'], $loginInfo['password'], true, true);
+                    return;
+                } elseif (isset($loginInfo['error'])) {
+                    appendErrorMessage($loginInfo['error']);
+                    return 1;
+                } else {
+                    return 1;
+                }
             }
         } catch (Exception $e) {
         }
