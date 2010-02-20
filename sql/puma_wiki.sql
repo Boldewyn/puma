68ecc73658bfc5d7958840b6ba43dc98dc34b315
Manuel Strehl <git@manuel-strehl.de>
2010-02-20 19:22:11 +0100

    SQL: added timestamps

--- sql/puma_wiki.sql
+++ sql/puma_wiki.sql
@@ -6,8 +6,7 @@ CREATE TABLE `puma_wiki_active` (
   `id` INT( 10 ) NOT NULL ,
   `item` VARCHAR( 255 ) NOT NULL ,
   PRIMARY KEY ( `id` , `item` ),
-  INDEX( item(10) ),
-  UNIQUE ( `id`, `item` )
+  INDEX( `item`(10) )
 );
 
 CREATE TABLE `puma_wiki_pages` (
@@ -18,8 +17,7 @@ CREATE TABLE `puma_wiki_pages` (
   `description` VARCHAR( 255 ) NOT NULL,
   `editor` INT( 10 ) NOT NULL ,
   `replaces` INT( 10 ) NULL DEFAULT NULL ,
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
   PRIMARY KEY ( `id` ),
-  INDEX ( `item` ),
-  UNIQUE ( `id` )
+  INDEX ( `item`(10) )
 );
