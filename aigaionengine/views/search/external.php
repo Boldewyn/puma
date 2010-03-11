a5dd79a7000343e712ccca05e64d4848b548f8f2
Manuel Strehl <git@manuel-strehl.de>
2010-03-11 12:52:05 +0100

    actualized external search engines

--- aigaionengine/views/search/external.php
+++ aigaionengine/views/search/external.php
@@ -12,54 +12,34 @@ if(! isset($searchengines)) { $searchengines = array(); }
   </fieldset>
   <script type="text/javascript">
   /*<![CDATA[*/
-    function searchExtern (action, method, parameters) {
-      var q = document.getElementById('search_external_query');
-      var f = q.form;
-      var hiddens = [];
-      f.method = method;
-      f.action = action;
-      f.target = "_blank";
-      f.encoding = "application/x-www-form-urlencoded;charset=utf-8";
-      for (var v in parameters) {
-        if (parameters[v].search(/{query}/) > -1) {
-          q.name = v;
-          q.value = parameters[v].replace(/{query}/, q.value);
-        } else {
-          hiddens.unshift(hidden(v, parameters[v]));
-          f.appendChild(hiddens[0]);
+    function search_external_submit (action, method, q, p) {
+      var $q = $('#search_external_query');
+      var $f = $('#search_external_form');
+      $f.attr('method', method)
+        .attr('action', action)
+        .attr('target', "_blank")
+        .attr('encoding', "application/x-www-form-urlencoded;charset=utf-8");
+      $q.attr('name', q);
+      if (p) {
+        for (var i = 0; i < p.length; i++) {
+          $f.append('<input type="hidden" name="'+p[i][0]+'" value="'+p[i][1].replace("{query}", $q.val())+'" />');
         }
       }
-      f.submit();
-      /** remove hidden inputs after submit */
-      while (hiddens.length > 0) {
-        hiddens[0].parentNode.removeChild(hiddens[0]);
-        hiddens.shift();
-      }
+      $f.submit();
+      $('input:hidden', $form).remove();
       return false;
-    }
-    
-    function hidden (name, value) {
-      var i = document.createElement("input");
-      i.type = "hidden";
-      i.name = name;
-      i.value = value;
-      return i;
     }
   /*   ]]>   */
   </script>
   <ul>
     <?php
       foreach ($searchengines as $e):
-        if (isset($e['active']) && $e['active'] == 1) :
-          $params = unserialize($e['parameters']);
-          $s = '{';
-          foreach ($params as $p => $v) {
-            $s .= "&apos;$p&apos; : &apos;$v&apos;,";
-          }
-          $s = substr($s, 0, -1) . '}';
-          ?>
+        if (isset($e['active']) && $e['active'] == 1) : ?>
           <li>
-            <button type="button" title="<?php echo $e['url'];?>" onclick="searchExtern('<?php echo $e['url'];?>', '<?php echo $e['method'];?>', <?php _h($s)?>);"><?php
+            <button type="button" title="<?php echo $e['url'];?>"
+                onclick="search_external_submit('<?php _h($e['url']) ?>', '<?php echo $e['method'];?>', '<?php echo $e['q'] ?>'
+                <?php if(isset($e['p'])) { echo ', ',h($e['p']); }?>
+                );"><?php
               if (isset ($e['image']) && $e['image'] != '') {
                 echo '<img src="',$e['image'],'" alt="',$e['name'],'" />';
               } else {
