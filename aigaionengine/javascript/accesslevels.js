792830595867fd652119507070953a1b90800267
Manuel Strehl <git@manuel-strehl.de>
2010-03-18 22:19:32 +0100

    another one deleted

--- aigaionengine/javascript/accesslevels.js
+++
@@ -1,4 +0,0 @@
-function submitAccessLevel(redirecturl, re,object_type, object_id) {
-    var dropdown = $(re+'_al_'+object_type+'_'+object_id);
-    window.location.href = (redirecturl+'/'+object_type+'/'+object_id+'/'+re+'/'+dropdown.value);
-}
