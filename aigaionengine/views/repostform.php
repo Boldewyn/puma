175cbc945b4a91115e9c840c38f1b9e77c2044f1
Manuel Strehl <git@manuel-strehl.de>
2010-03-19 15:32:32 +0100

    repostform: cleaned code, perhaps introduced an error (see comment in view file)

--- aigaionengine/views/repostform.php
+++ aigaionengine/views/repostform.php
@@ -3,27 +3,29 @@
 
 $userlogin = getUserLogin();
 $this->load->helper('form');
-echo "<div class='editform'>";
-echo form_open($this->latesession->get('FORMREPOST_uri'));
-echo sprintf(__("The system detected that you were logged out while submitting a form named '%s'. The data in that form has <b>not</b> yet been submitted successfully to the database. Press the button below to re-submit the form data."), $this->latesession->get('FORMREPOST_formname'))."<br/><br/>";
-if ($userlogin->isAnonymous()) {
-    echo "<div class='errormessage'>";
-    echo "<b>".__('NOTE')."</b>: ".__("You are now logged in as a guest user. If you submitted the form from a registered account, you should first login with your registered account before resubmitting the form, because from this guest account you might then not have enough rights to submit the form.")."<br/><br/>";
-    echo "</div>";
-}
+echo form_open($this->latesession->get('FORMREPOST_uri')); ?>
+  <p>
+    <?php printf(__('The system detected that you were logged out while submitting '.
+      'a form named &ldquo;%s&rdquo;. The data in that form has <strong>not</strong> '.
+      'yet been submitted successfully to the database. Press the button below to '.
+      're-submit the form data.'), $this->latesession->get('FORMREPOST_formname')) ?>
+  </p>
+  <?php if ($userlogin->isAnonymous()): ?>
+  <p class="error">
+    <strong><?php _e('NOTE:') ?></strong> <?php _e('You are now logged in as a guest '.
+      'user. If you submitted the form from a registered account, you should first '.
+      'log in with your registered account before resubmitting the form, because from '.
+      'this guest account you might then not have enough rights to submit the form.') ?>
+  </p>
+  <?php endif; ?>
+  <p>
+    <input type="submit" class="submit" name="repost_form" value="<?php _e('Repost')?>" />
+    <?php foreach($this->latesession->get('FORMREPOST_post') as $field=>$val): ?>
+      <input type="hidden" name="<?php _h($field)?>" value="<?php _h($val)?>" />
+    <?php endforeach ?>
+    <input type="hidden" name="form_reposted" value="form_reposted" />
+    <?php /* Ui, ui. This might perhaps not work. In the original code, the cancel button was an empty request w/o hidden fields to base_url() */ ?>
+    <a href="<?php echo base_url()?>" class="pseudobutton"><?php _e('Cancel')?></a>
+  </p>
+</form>
 
-echo form_submit('repost_form', __('Repost'));
-foreach($this->latesession->get('FORMREPOST_post') as $field=>$val) {
-    echo form_hidden($field,$val);
-}
-echo form_hidden('form_reposted','form_reposted');
-echo form_close();
-echo form_open('');
-echo form_submit('cancel', __('Cancel'));
-echo form_hidden('form_reposted','form_reposted');
-echo form_close();
-echo "</div>";
-//note: if the form was a search form, we might as well immediately force a submit through javascript; searches don't need confirmation
-
-//reset the session vars related to this formrepost, but not here because a refresh would then destroy this form
-?>
