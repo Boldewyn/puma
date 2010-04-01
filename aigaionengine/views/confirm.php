77bb0ce113a8e32b25741f9e89b32d6df0c69cd3
Manuel Strehl <git@manuel-strehl.de>
2010-04-01 16:20:52 +0200

    replaced controller specific delete views with a generic confirm view

---
+++ aigaionengine/views/confirm.php
@@ -0,0 +1,14 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<form method="post" action="<?php echo site_url($url) ?>" class="confirmform">
+  <p>
+    <?php echo $question ?>
+  </p>
+  <p>
+    <input type="submit" class="submit standard_input" value="<?php _e('Confirm') ?>" />
+    <?php _a((isset($cancel_url)? $cancel_url : ''), __('Cancel'), 'class="pseudobutton standard_input"') ?>
+  </p>
+</form>
+<?php if (isset($additional_info)):
+  echo $additional_info;
+endif; ?>
+
